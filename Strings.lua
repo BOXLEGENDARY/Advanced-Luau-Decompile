277ffdc171e774e12a02d4b560671b35f63f86ba
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2025-06-12 07:54:57 +0700

    Update Strings.lua

--- Strings.lua
+++ Strings.lua
@@ -2,9 +2,9 @@
 local MemeStrings = {
 	" DECOMPILED BY ADVANCED DECOMPILER",
 	" DISASSEMBLED...",
-	"decompiler is slow, removed right now :(",
-	"Beta Release By ZxL may has Error",
-	"Upgrade By ZxL",
+	" decompiler is slow, removed right now :(",
+	" Beta Release By ZxL may has Error",
+	" Upgrade By ZxL",
 	" " .. os.date(),
 	" your advertisement could be here"
 }
