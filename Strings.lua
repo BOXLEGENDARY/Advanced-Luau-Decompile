d9eed6294f26112b38bb31ea2e67b31565e5db0a
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2025-06-15 04:18:07 +0700

    Add files via upload

--- Strings.lua
+++ Strings.lua
@@ -1,21 +1,20 @@
--- Collection of strings later formatted by the decompiler for output
+math.randomseed(os.time())
+
+local CurrentDate = os.date()
+
 local MemeStrings = {
-	" DECOMPILED BY ADVANCED DECOMPILER",
-	" DISASSEMBLED...",
-	"decompiler is slow, removed right now :(",
-	"Beta Release By ZxL may has Error",
-	"Upgrade By ZxL",
-	" " .. os.date(),
-	" your advertisement could be here"
+  "Beta Release by ZxL - May Have Bugs",
+  "Upgraded by ZxL",
+  " " .. CurrentDate
 }
 
 local Strings = {
-	SUCCESS = "--" .. MemeStrings[math.random(#MemeStrings)] .. "\n%s",
-	TIMEOUT = "-- DECOMPILER TIMEOUT",
-	COMPILATION_FAILURE = "-- SCRIPT FAILED TO COMPILE, ERROR:\n%s",
-	UNSUPPORTED_LBC_VERSION = "-- PASSED BYTECODE IS TOO OLD AND IS NOT SUPPORTED",
-	USED_GLOBALS = "-- USED GLOBALS: %s.\n",
-	DECOMPILER_REMARK = "-- DECOMPILER REMARK: %s\n"
+  SUCCESS = "-- " .. MemeStrings[math.random(#MemeStrings)] .. "\n%s",
+  TIMEOUT = "-- DECOMPILER TIMEOUT",
+  COMPILATION_FAILURE = "-- SCRIPT FAILED TO COMPILE, ERROR:\n%s",
+  UNSUPPORTED_LBC_VERSION = "-- PASSED BYTECODE IS TOO OLD AND IS NOT SUPPORTED",
+  USED_GLOBALS = "-- USED GLOBALS: %s.\n",
+  DECOMPILER_REMARK = "-- DECOMPILER REMARK: %s\n"
 }
 
 return Strings
