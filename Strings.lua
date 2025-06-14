eb53d7a68504abd178cf3dff97ce98770898418d
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2025-06-15 06:35:01 +0700

    Add files via upload

--- Strings.lua
+++ Strings.lua
@@ -1,12 +1,8 @@
 -- Collection of strings later formatted by the decompiler for output
 local MemeStrings = {
-	" DECOMPILED BY ADVANCED DECOMPILER",
-	" DISASSEMBLED...",
-	" decompiler is slow, removed right now :(",
-	" Beta Release By ZxL may has Error",
-	" Upgrade By ZxL",
+	"Beta Release By ZxL - Expect Bugs",
+	"Upgrade By ZxL",
 	" " .. os.date(),
-	" your advertisement could be here"
 }
 
 local Strings = {
