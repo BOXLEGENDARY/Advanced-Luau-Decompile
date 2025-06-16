13345b99c57e7b3f48f1aec8249de3293189100f
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2025-06-16 20:43:31 +0700

    Add files via upload

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ local LIST_USED_GLOBALS = false -- list all (non-Roblox!!) globals used in the s
 local RETURN_ELAPSED_TIME = false -- return time it took to finish processing the bytecode
 local DECODE_AS_BASE64 = false -- Decodes the bytecode as base64 if it's returned as such.
 local USE_IN_STUDIO = false -- Toggles Roblox Studio mode, which allows for this to be used in
-local Debug = false -- true / show all debug loading in console | false / show load and success only
+local Debug = true -- true / show all debug loading in console | false / show load and success only
 local GitHubUrlShow = false
 
 -- For studio, put your bytecode here.
