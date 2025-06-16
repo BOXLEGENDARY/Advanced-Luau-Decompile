3eeb168cace7627bb9ae9b46aee08acece428d6d
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2025-06-16 18:02:48 +0700

    Add files via upload

--- Strings.lua
+++ Strings.lua
@@ -1,17 +1,6 @@
 -- Collection of strings later formatted by the decompiler for output
 local MemeStrings = {
-	" boo 👻",
-	" Exceeded decompiler timeout.",
-	" DECOMPILED BY ADVANCED DECOMPILER V3",
-	" DISASSEMBLED...",
-	" Decompiled with the Synapse X Luau decompiler.",
-	" 𝓓𝓮𝓬𝓸𝓶𝓹𝓲𝓵𝓮𝓭 𝓫𝔂 𝓯𝓻𝓮𝓪𝓴𝔂 𝓭𝓮𝓬𝓸𝓶𝓹𝓲𝓵𝓮𝓻",
-	"SynapseX Decompiler",
-	"decompiler is slow, removed right now :(",
-	"NOTE: Currently in beta! Not representative of final product.",
-	" params : ...",
-	" " .. os.date(),
-	" your advertisement could be here"
+	" Beta Release By ZxL",
 }
 
 local Strings = {
