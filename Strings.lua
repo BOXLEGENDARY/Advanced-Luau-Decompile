341f37409748647414895ac8e2f30c57a70dcfd0
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2025-07-26 08:38:06 +0700

    Add files via upload

--- Strings.lua
+++ Strings.lua
@@ -1,15 +1,14 @@
--- Collection of strings later formatted by the decompiler for output
-local MemeStrings = {
-	" Decompline By ZxL",
+local BaseStrings = {
+ " Decompline By ZxL",
 }
 
 local Strings = {
-	SUCCESS = "--" .. MemeStrings[math.random(#MemeStrings)] .. "\n%s",
-	TIMEOUT = "-- DECOMPILER TIMEOUT",
-	COMPILATION_FAILURE = "-- SCRIPT FAILED TO COMPILE, ERROR:\n%s",
-	UNSUPPORTED_LBC_VERSION = "-- PASSED BYTECODE IS TOO OLD AND IS NOT SUPPORTED",
-	USED_GLOBALS = "-- USED GLOBALS: %s.\n",
-	DECOMPILER_REMARK = "-- DECOMPILER REMARK: %s\n"
+ SUCCESS = "--" .. BaseStrings[math.random(#BaseStrings)] .. "\n%s",
+ TIMEOUT = "-- DECOMPILER TIMEOUT",
+ COMPILATION_FAILURE = "-- SCRIPT FAILED TO COMPILE, ERROR:\n%s",
+ UNSUPPORTED_LBC_VERSION = "-- PASSED BYTECODE IS TOO OLD AND IS NOT SUPPORTED",
+ USED_GLOBALS = "-- USED GLOBALS: %s.\n",
+ DECOMPILER_REMARK = "-- DECOMPILER REMARK: %s\n"
 }
 
 return Strings
