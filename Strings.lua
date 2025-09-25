ff2ad940e49f8db554a3234fbe4da2d4d5c43c75
ZxL <107736809+BOXLEGENDARY@users.noreply.github.com>
2025-09-26 02:40:51 +0700

    Add files via upload

--- Strings.lua
+++ Strings.lua
@@ -1,14 +1,17 @@
 local BaseStrings = {
- " Decompiler By ZxL ( Advanced Luau Decompiler )",
+    " " .. os.date(),
+    " Decompiler By ZxL ( Advanced Luau Decompiler )",
 }
 
+local CombinedBaseString = table.concat(BaseStrings, "\n")
+
 local Strings = {
- SUCCESS = "--" .. BaseStrings[math.random(#BaseStrings)] .. "\n%s",
- TIMEOUT = "-- DECOMPILER TIMEOUT",
- COMPILATION_FAILURE = "-- SCRIPT FAILED TO COMPILE, ERROR:\n%s",
- UNSUPPORTED_LBC_VERSION = "-- PASSED BYTECODE IS TOO OLD AND IS NOT SUPPORTED",
- USED_GLOBALS = "-- USED GLOBALS: %s.\n",
- DECOMPILER_REMARK = "-- DECOMPILER REMARK: %s\n"
+    SUCCESS = "--" .. CombinedBaseString .. "\n%s",
+    TIMEOUT = "-- DECOMPILER TIMEOUT",
+    COMPILATION_FAILURE = "-- SCRIPT FAILED TO COMPILE, ERROR:\n%s",
+    UNSUPPORTED_LBC_VERSION = "-- PASSED BYTECODE IS TOO OLD AND IS NOT SUPPORTED",
+    USED_GLOBALS = "-- USED GLOBALS: %s.\n",
+    DECOMPILER_REMARK = "-- DECOMPILER REMARK: %s\n"
 }
 
 return Strings
